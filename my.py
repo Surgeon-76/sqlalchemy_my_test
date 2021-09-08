f82cc470f5b8ab85c18f0ac87c1d7da65f31922e
Surgeon76 <Surgeon76@yandex.ru>
2021-09-08 21:37:02 +0300

    создание моделей таблтиц

--- my.py
+++ my.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, MetaData, Table, Integer,\
     String, Column, DateTime, ForeignKey, Numeric, SmallInteger
-import sqlalchemy
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -11,7 +10,7 @@ user_name_db = 'surglin'
 password_db = 'Nusha230399'
 db_name = 'sqlalchemy_test'
 
-engine = sqlalchemy.create_engine(f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}")
+engine = create_engine(f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}")
 
 
 Base = declarative_base()
@@ -29,4 +28,30 @@ class Cusomer(Base):
     orders = relationship('Order', backref='customer')
 
 
-    
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(200), nullable=False)
+    cost_price = Column(Numeric(10, 2), nullable=False)
+    selling_price = Column(Numeric(10, 2), nullable=False)
+    quantity = Column(Integer())
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer(), primary_key=True)
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+    date_placed = Column(DateTime(), default=datetime.now)
+    line_items = relationship("OrderLine", backref='order')
+
+
+class OrderLine(Base):
+    __tablename__ = 'arder_lines'
+    id = Column(Integer(), primary_key=True)
+    order_id = Column(Integer(), ForeignKey('orders.id'))
+    item_id = Column(Integer(), ForeignKey('items.id'))
+    quantity = Column(SmallInteger())
+    item = relationship("Item")
+
+
+Base.metadata.create_all(engine)
