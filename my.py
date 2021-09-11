e7a8ca8b29188ac5fe8d3aa4555c28758b206a2d
Surgeon76 <Surgeon76@yandex.ru>
2021-09-11 12:17:02 +0300

    Заработало!

--- my.py
+++ my.py
@@ -12,7 +12,7 @@ db_name = 'sqlalchemy_test'
 
 engine = create_engine(f"postgresql://{user_name_db}:{password_db}@localhost/{db_name}", echo=True)
 
-session = sessionmaker(bind=engine)
+session = sessionmaker(autoflush=False, bind=engine)
 
 Base = declarative_base()
 
@@ -26,33 +26,63 @@ class Customer(Base):
     email = Column(String(200), nullable=False)
     created_on = Column(DateTime(), default=datetime.now)
     updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
-    orders = relationship('Order', backref='customer')
+    # orders = relationship("Order", backref='customer')
 
 
-class Item(Base):
-    __tablename__ = 'items'
-    id = Column(Integer(), primary_key=True)
-    name = Column(String(200), nullable=False)
-    cost_price = Column(Numeric(10, 2), nullable=False)
-    selling_price = Column(Numeric(10, 2), nullable=False)
-    quantity = Column(Integer())
+# class Item(Base):
+#     __tablename__ = 'items'
+#     id = Column(Integer(), primary_key=True)
+#     name = Column(String(200), nullable=False)
+#     cost_price = Column(Numeric(10, 2), nullable=False)
+#     selling_price = Column(Numeric(10, 2), nullable=False)
+#     quantity = Column(Integer())
 
 
-class Order(Base):
-    __tablename__ = 'orders'
-    id = Column(Integer(), primary_key=True)
-    customer_id = Column(Integer(), ForeignKey('customers.id'))
-    date_placed = Column(DateTime(), default=datetime.now)
-    line_items = relationship("OrderLine", backref='order')
+# class Order(Base):
+#     __tablename__ = 'orders'
+#     id = Column(Integer(), primary_key=True)
+#     customer_id = Column(Integer(), ForeignKey('customers.id'))
+#     date_placed = Column(DateTime(), default=datetime.now)
+#     line_items = relationship("OrderLine", backref='order')
 
 
-class OrderLine(Base):
-    __tablename__ = 'order_lines'
-    id = Column(Integer(), primary_key=True)
-    order_id = Column(Integer(), ForeignKey('orders.id'))
-    item_id = Column(Integer(), ForeignKey('items.id'))
-    quantity = Column(SmallInteger())
-    item = relationship("Item")
+# class OrderLine(Base):
+#     __tablename__ = 'order_lines'
+#     id = Column(Integer(), primary_key=True)
+#     order_id = Column(Integer(), ForeignKey('orders.id'))
+#     item_id = Column(Integer(), ForeignKey('items.id'))
+#     quantity = Column(SmallInteger())
+#     item = relationship("Item")
 
+#session = Session()
+
+db  = session()
 
 Base.metadata.create_all(engine)
+
+
+
+c1 = Customer(
+    first_name = 'Dmitriy',
+    last_name = 'Yatsenko',
+    username = 'Moseend',
+    email = 'moseend@mail.com'
+)
+
+# c2 = Customer(
+#     first_name = 'Valeriy',
+#     last_name = 'Golyshkin',
+#     username = 'Fortioneaks',
+#     email = 'fortioneaks@gmail.com'
+# )
+
+print(c1.first_name)
+
+db.add(c1)
+#session.add(c2)
+
+print(db.new)
+
+#Base.metadata.create_all(engine)
+db.commit()
+db.close()
